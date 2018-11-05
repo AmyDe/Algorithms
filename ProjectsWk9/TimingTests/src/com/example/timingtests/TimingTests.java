7c963c1c9885f98d26243f52e62ac40ee46f9626
Amy Denny-Guerin <amydenny-guerin@Amys-MacBook-Pro.local>
2018-11-05 15:46:29 +0000

    Testing time taken to shuffle arrays of different lengths

--- ProjectsWk9/TimingTests/src/com/example/timingtests/TimingTests.java
+++ ProjectsWk9/TimingTests/src/com/example/timingtests/TimingTests.java
@@ -1,5 +1,11 @@
 package com.example.timingtests;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class TimingTests {
 
     public static void main(String[] args) {
@@ -8,49 +14,30 @@ public class TimingTests {
         for (int i = 0; i < tenArray.length; i++) {
             tenArray[i] = i + 1;
         }
-        for (int i = 0; i < tenArray.length/2; i++) {
-            int temp = tenArray[i];
-            tenArray[i] = tenArray[tenArray.length -i -1];
-            tenArray[tenArray.length -i -1] = temp;
-        }
 
-        int[] hundArray = new int[100];
-        for (int i = 0; i < hundArray.length; i++) {
-            hundArray[i] = i + 1;
-        }
-        for (int i = 0; i < hundArray.length/2; i++) {
-            int temp = hundArray[i];
-            hundArray[i] = hundArray[tenArray.length -i -1];
-            hundArray[hundArray.length -i -1] = temp;
-        }
-
-        int[] thouArray = new int[1000];
-        for (int i = 0; i < thouArray.length; i++) {
-            thouArray[i] = i + 1;
-        }
-        for (int i = 0; i < thouArray.length/2; i++) {
-            int temp = thouArray[i];
-            thouArray[i] = thouArray[thouArray.length -i -1];
-            thouArray[tenArray.length -i -1] = temp;
-        }
-
-        int[] milArray = new int[1000000];
-        for (int i = 0; i < milArray.length; i++) {
-            milArray[i] = i + 1;
-        }
-        for (int i = 0; i < milArray.length/2; i++) {
-            int temp = milArray[i];
-            milArray[i] = milArray[tenArray.length -i -1];
-            milArray[milArray.length -i -1] = temp;
-        }
+//        int[] hundArray = new int[100];
+//        for (int i = 0; i < hundArray.length; i++) {
+//            hundArray[i] = i + 1;
+//        }
+//
+//
+//        int[] thouArray = new int[1000];
+//        for (int i = 0; i < thouArray.length; i++) {
+//            thouArray[i] = i + 1;
+//        }
+////
+//        int[] milArray = new int[1000000];
+//        for (int i = 0; i < milArray.length; i++) {
+//            milArray[i] = i + 1;
+//        }
 
         long startTime = System.nanoTime();
 
-        int test = tenArray[tenArray.length-1];
+        Collections.shuffle(Arrays.asList(tenArray));
 
         long endTime = System.nanoTime();
         long timeTaken = endTime - startTime;
 
-        System.out.println("Time taken to reverse 10 object array in n/s: " + timeTaken);
+        System.out.println("Time taken to shuffle 10 object array in n/s: " + timeTaken);
     }
 }
