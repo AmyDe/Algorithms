ab947cd2554c78125247e5d436ad2406d1c470df
Amy Denny-Guerin <amydenny-guerin@Amys-MacBook-Pro.local>
2018-11-06 11:15:32 +0000

    Created automated testing for sort for 100 arrays, incrementing by 100

--- ProjectsWk9/TimingTests/src/com/example/timingtests/TimingTests.java
+++ ProjectsWk9/TimingTests/src/com/example/timingtests/TimingTests.java
@@ -1,43 +1,32 @@
 package com.example.timingtests;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class TimingTests {
 
     public static void main(String[] args) {
 
-        int[] tenArray = new int[10];
-        for (int i = 0; i < tenArray.length; i++) {
-            tenArray[i] = i + 1;
+        for (int size = 10; size < 10000; size += (10000/100)) {
+            int[] testArr = createShufArray(size);
+
+            long startTime = System.nanoTime();
+
+            Arrays.sort(testArr);
+
+            long endTime = System.nanoTime();
+            long timeTaken = endTime - startTime;
+
+            System.out.println(size + "\t" + timeTaken);
         }
+    }
 
-//        int[] hundArray = new int[100];
-//        for (int i = 0; i < hundArray.length; i++) {
-//            hundArray[i] = i + 1;
-//        }
-//
-//
-//        int[] thouArray = new int[1000];
-//        for (int i = 0; i < thouArray.length; i++) {
-//            thouArray[i] = i + 1;
-//        }
-////
-//        int[] milArray = new int[1000000];
-//        for (int i = 0; i < milArray.length; i++) {
-//            milArray[i] = i + 1;
-//        }
-
-        long startTime = System.nanoTime();
-
-        Collections.shuffle(Arrays.asList(tenArray));
-
-        long endTime = System.nanoTime();
-        long timeTaken = endTime - startTime;
-
-        System.out.println("Time taken to shuffle 10 object array in n/s: " + timeTaken);
+    private static int[] createShufArray(int size) {
+        int[] myArr = new int[size];
+        for (int i = 0; i < size; i++) {
+            myArr[i] = i + 1;
+        }
+        Collections.shuffle(Arrays.asList(myArr));
+        return myArr;
     }
 }
